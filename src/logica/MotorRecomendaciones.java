523c1c5333701f99f8352df6d11cde7c09bd1fb6
Edward M. <122883489+Osmanv04@users.noreply.github.com>
2026-08-06 10:08:12 -0400

    cambios del profesor

--- src/logica/MotorRecomendaciones.java
+++ src/logica/MotorRecomendaciones.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package logica;
 
 import datos.GestorCatalogo;
@@ -115,5 +116,72 @@ public class MotorRecomendaciones {
     }
     public void limpiarMemoria() {
         memoriaActiva.clear();
+=======
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+class Pelicula {
+    String titulo;
+    String[] palabrasClave;
+
+    public Pelicula(String titulo, String[] palabrasClave) {
+        this.titulo = titulo;
+        this.palabrasClave = palabrasClave;
+    }
+}
+
+public class MotorRecomendaciones {
+
+    private List<Pelicula> catalogo;
+    
+    private HashSet<String> memoriaActiva;
+
+    public MotorRecomendaciones() {
+        this.catalogo = new ArrayList<>();
+        this.memoriaActiva = new HashSet<>();
+        cargarCatalogo();
+    }
+
+    private void cargarCatalogo() {
+        catalogo.add(new Pelicula("Matrix", new String[]{"accion", "ciencia", "ficcion", "hacker"}));
+        catalogo.add(new Pelicula("El Conjuro", new String[]{"terror", "miedo", "fantasmas", "suspenso"}));
+        catalogo.add(new Pelicula("Superbad", new String[]{"comedia", "risa", "divertido", "humor"}));
+    }
+
+    public String buscarMejorPelicula(String textoProcesado) {
+        String[] palabrasUsuario = textoProcesado.split(" ");
+
+        Pelicula mejorPelicula = null;
+        int maxPuntos = 0;
+
+        for (Pelicula peli : catalogo) {
+
+            if (memoriaActiva.contains(peli.titulo)) {
+                continue;
+            }
+
+            int puntosActuales = 0;
+            for (String palabraClave : peli.palabrasClave) {
+                for (String palabraUsuario : palabrasUsuario) {
+                    if (palabraUsuario.equalsIgnoreCase(palabraClave)) {
+                        puntosActuales++;
+                    }
+                }
+            }
+
+            if (puntosActuales > maxPuntos) {
+                maxPuntos = puntosActuales;
+                mejorPelicula = peli;
+            }
+        }
+
+        if (maxPuntos > 0 && mejorPelicula != null) {
+            memoriaActiva.add(mejorPelicula.titulo);
+            return "Te recomiendo ver '" + mejorPelicula.titulo + "'! (Coincidencias: " + maxPuntos + ")";
+        } else {
+            return "No encontré opciones nuevas para lo que buscas o ya te recomendé las películas disponibles en esa categoría.";
+        }
+>>>>>>> Stashed changes
     }
 }
